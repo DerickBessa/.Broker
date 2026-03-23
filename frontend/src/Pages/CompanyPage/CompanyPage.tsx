ddbf5d5116a7f9f2c2fcb3631439952eca3cc755
Derick Bessa de Almeida <derickbessa12@gmail.com>
2026-03-23 13:31:31 -0300

    fix: babel dependency

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { CompanyProfile } from "../../company";
+import { CompanyProfile as CompanyProfileType } from "../../company";
 import { Link, useParams } from "react-router-dom";
 import { getCompanyProfile } from "../../api";
 import Sidebar from "../../Components/Sidebar/Sidebar";
@@ -7,21 +7,25 @@ import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard
 import Tile from "../../Components/Tile/Tile";
 import Spinner from "../../Components/Spinners/Spinner";
 import CompFinder from "../../Components/CompFinder/CompFinder";
+import CompanyProfile from "../../Components/CompanyProfile/CompanyProfile";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
   let { ticker } = useParams();
 
-  const [company, setCompany] = useState<CompanyProfile>();
+  const [company, setCompany] = useState<CompanyProfileType | null>(null);
 
   useEffect(() => {
-    const getProfileInit = async () => {
-      const result = await getCompanyProfile(ticker!);
-      setCompany(result?.data);
-    };
+  const getProfileInit = async () => {
+    const result = await getCompanyProfile(ticker!);
+    setCompany(result?.data ?? null);
+  };
+
+  if (ticker) {
     getProfileInit();
-  }, []);
+  }
+}, [ticker]);
 
   return (
     <>
@@ -35,6 +39,7 @@ const CompanyPage = (props: Props) => {
 			<Tile title="Exchange" subTitle={company.exchange} />
 			<Tile title="IPO Date" subTitle={company.ipo} />
 			<Tile title="Website" subTitle={company.weburl} />
+			<CompanyProfile />
 			<CompFinder ticker={company.ticker} />
             <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
 			{company.name} — {company.finnhubIndustry} | {company.exchange}
